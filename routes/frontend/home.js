b3dd40cc15d27d7c3590fc71cd44460c3fef33a2
payalraghuvanshi09 <payal.exergy@gmail.com>
2022-10-15 15:25:03 +0530

    final submit

---
+++ routes/frontend/home.js
@@ -0,0 +1,21 @@
+var express = require("express");
+const homeController = require("../../controller/homeController");
+var router = express.Router();
+
+router.get("/fhome",homeController.Image);
+router.get("/fbrands",homeController.Brands)
+router.get("/fclients",homeController.Clients)
+router.get("/fproducts",homeController.Products)
+router.get("/fcontact",homeController.Contact)
+router.get("/about",homeController.About)
+router.get("/",homeController.Home)
+// router.get("/active",homeController.Active)
+// router.get("/mission",homeController.Mission)
+
+// router.post("/upload",homeController.Upload)
+
+
+
+
+
+module.exports = router;
