a8c7a4a38dff04ed6d9567b99d6a2b173c806f7c
payalraghuvanshi09 <payal.exergy@gmail.com>
2022-10-15 16:35:42 +0530

    changes

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
 const { createPool } = require("mysql");
 
 const pool = createPool({
-  port: process.env.DB_PORT,
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.MYSQL_DB,
+  port: 5000,
+  host: "localhost",
+  user: root,
+  password: "",
+  database: fiducia,
 });
 
 module.exports = pool;
