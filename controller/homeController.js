507b9cede419295fc45658f4f3bea99e1fb43e0c
payalraghuvanshi09 <payal.exergy@gmail.com>
2022-10-17 16:15:46 +0530

    changes

--- controller/homeController.js
+++ controller/homeController.js
@@ -42,7 +42,7 @@ module.exports = {
       }
       console.log("res=>>>>>", results);
       // var imgsrc = 'process.env.baseUrl' + req.file.baseUrl
-      res.render("pages/frontend/brands", { title: "Express", data: results, active_nav:"fbrands"});
+      res.render("pages/frontend/brands", { title: "Express", data: results,active_nav: "fbrands" });
     });
   },
 
