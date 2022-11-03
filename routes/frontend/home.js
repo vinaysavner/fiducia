f596faa179d298ce1dcffe49db0041e76fa50230
payalraghuvanshi09 <payal.exergy@gmail.com>
2022-11-03 15:00:42 +0530

    changes

--- routes/frontend/home.js
+++ routes/frontend/home.js
@@ -2,12 +2,13 @@ var express = require("express");
 const homeController = require("../../controller/homeController");
 var router = express.Router();
 
+
 router.get("/fhome",homeController.Image);
-router.get("/fbrands",homeController.Brands)
-router.get("/fclients",homeController.Clients)
-router.get("/fproducts",homeController.Products)
-router.get("/fcontact",homeController.Contact)
-router.get("/about",homeController.About)
+router.get("/Brand",homeController.Brands)
+router.get("/Client",homeController.Clients)
+router.get("/Product",homeController.Products)
+router.get("/Contact",homeController.Contact)
+router.get("/About",homeController.About)
 router.get("/",homeController.Home)
 // router.get("/active",homeController.Active)
 // router.get("/mission",homeController.Mission)
